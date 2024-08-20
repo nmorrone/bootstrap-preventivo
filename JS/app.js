51cf45fb8efd82027262bfc62abf10ff100b895c
nmorrone <n.morrone96@gmail.com>
2024-08-20 12:13:17 +0200

    js-start-file

--- JS/app.js
+++ JS/app.js
@@ -2,3 +2,10 @@ console.log('Bootstrap - Preventivo')
 console.log('Benvenuto')
 
 //annotazioni utili
+/* l'utente inserisci il tipo di lavoro, il cui prezzo orario varia
+in base a quello scelto, inserisce il quantitativo di ore e chiede di
+ricevere un preventivo*/
+/*N.B. può utilizzare un codice coupon per ricevere un eventuale sconto */
+
+let price = 0
+
