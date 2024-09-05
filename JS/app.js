fcdb092582edcc9e4037ce9b39fe6aff1869de90
nmorrone <n.morrone96@gmail.com>
2024-09-05 19:08:41 +0200

    coupon-array

--- JS/app.js
+++ JS/app.js
@@ -22,6 +22,12 @@ const hoursElement = document.getElementById('number')
 
 const couponElement = document.getElementById('coupon')
 
+/* creo l'array contentente tutti i codici coupon
+che l'utente potrebbe inserire per lo sconto */
+
+3
+let coupons = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
+
 /* una volta creati gli elementi acquisisco i valori che avranno
 ,in base all'interazione dell'utente con il form, tramite la funzione
 submit quando l'utente "invia" i valori */
@@ -31,21 +37,45 @@ formElement.addEventListener('submit', function (event) {
     event.preventDefault()
 
     //recupero i valori inseriti
-    const selectedWork = workElement.value
+    let selectedWork = parseInt(workElement.value)
 
-    const numberHours = hoursElement.value
+    const numberHours = parseInt(hoursElement.value)
 
-    const coupon = couponElement.value
+    const coupon = couponElement.value.toUpperCase()
 
     //verifico funzionamento acquisizione dati
     console.log(selectedWork, numberHours, coupon)
 
 
+    /* inserisco la condizione per il quale inserisco l'importo
+    orario da moltiplicare alle ore per il calcolo, sulla base del
+    lavoro scelto dall'utente sul form */
+    if (selectedWork === 1) {
+
+        selectedWork = 20.50
+    }
+
+    else if (selectedWork === 2) {
+
+        selectedWork = 15.30
+    }
+
+    else {
+
+        selectedWork = 33.60
+    }
+
+    console.log(selectedWork)
+
+    //calcolo la bozza del prezzo senza coupon
+    price = selectedWork * numberHours
+    //verifico funzionamento
+    console.log(price)
 
 
+}
+)
 
-
-})
 
 
 
